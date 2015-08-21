b7b47ed8aeb30b72ccfe7ad38b555821fcf87d4a
KittiyaWarisri <Kittiya000preaw@gmail.com>
2015-08-21 17:21:30 +0700

    570610550 for third assignment

--- Lab/Lab1-1/Program.cs
+++ Lab/Lab1-1/Program.cs
@@ -10,23 +10,24 @@ namespace Lab1_1
     {
         static void Main(string[] args)
         {
-            bool flag = true;
+            //bool flag = true;
 
             // input
             int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
 
             // process
-            while (flag)
+            //while (flag)
+            for (int j = 0; j != input.Length - 1; j++)
             {
-                flag = false;
-                for (int i = 0; i != input.Length - 1; i++)
+                //flag = false;
+                for (int i = 0; i != input.Length  - j -1; i++)
                 {
-                    if (input[i] < input[i + 1])
+                    if (input[i] > input[i + 1])
                     {
                         int temp = input[i];
                         input[i] = input[i + 1];
                         input[i + 1] = temp;
-                        flag = true;
+                        //flag = true;
                     }
                 }
             }

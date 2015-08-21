b9fa66c03ab3890c30dda285468133bdb18dc933
Watchakon <WatchakonP@users.noreply.github.com>
2015-08-21 16:45:37 +0700

    Lab1.1 Remove flag
    570610600

--- Lab/Lab1-1/Program.cs
+++ Lab/Lab1-1/Program.cs
@@ -10,15 +10,14 @@ namespace Lab1_1
     {
         static void Main(string[] args)
         {
-            bool flag = true;
+            
 
             // input
             int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
 
             // process
-            while (flag)
-            {
-                flag = false;
+           for (int i = 0; i = input.Length - 1; i++)            
+           {   
                 for (int i = 0; i != input.Length - 1; i++)
                 {
                     if (input[i] < input[i + 1])

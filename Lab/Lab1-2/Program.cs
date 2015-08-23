ddbedab4fa8f845d177ef27c955a4ebe6c5cd0a3
Watchakon <WatchakonP@users.noreply.github.com>
2015-08-24 00:06:03 +0700

    Lab1.2 Input from command line

--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -7,52 +7,58 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 
-using System;
-
 // Program start class
 namespace Lab1_2
 {
-class BubbleSortFunction
+    class BubbleSortFunction
     {
-	static void Main(string[] args)
+        static void Main(string[] args)
         {
-        	Input();
+            Input();
         }
-/// //////////////////////////////////////
-	static void Input(){
+ ///////////////////////////////////////////////      
+        static void Input(){
             // input
-            int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
-            process(input);
+            string myInput;
+            int Num;
+            int[] input;
+            Console.Write("How many input number?: ");
+            myInput = Console.ReadLine();
+            Num = Int32.Parse(myInput);
+            input = new int[Num];
+        // Input process
+        	for (int i = 0; i != input.Length; i++){
+        		Console.Write("Input Number: ");
+        		input[i]=Int32.Parse(Console.ReadLine());        	
+        	}
+        process(input);
         }
-     /////////////////////////////////   
-	static void process(int[] input){
-            // process
-            bool flag = true;
-            while (flag)
-            {
-                flag = false;
-                for (int i = 0; i != input.Length - 1; i++)
-                {
-                    if (input[i] > input[i + 1])
-                    {
-                        int temp = input[i];
-                        input[i] = input[i + 1];
-                        input[i + 1] = temp;
-                        flag = true;
-                    }
-                }                
-            }
-            output(input);
+  ////////////////////////////////////////////////      
+  static void process(int[] input){
+	        // Sort process
+	        bool flag = true;
+	        while (flag){
+	            flag = false;
+	             for (int i = 0; i != input.Length - 1; i++){
+	                	if (input[i] > input[i + 1]){
+	                        int temp = input[i];
+	                        input[i] = input[i + 1];
+	                        input[i + 1] = temp;
+	                        flag = true;
+	                	}
+	             }
+	         }
+	        output(input);
         }
-       /////////////////////////////////
-	static void output(int[] input){
-            // output
+    ////////////////////////////////////////////////    
+    static void output(int[] input){
+         // output
             for (int i = 0; i != input.Length; i++)
             {
                 Console.Write(input[i]);
                 Console.Write(" ");
             }
             Console.ReadKey();
-        }
+        }     
     }
 }

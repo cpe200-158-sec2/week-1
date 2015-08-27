391ffa15e1a954c3b87a8d8e300109bed7044d6f
Peeranut Pongpakatien <peeranut.31@hotmail.com>
2015-08-27 16:02:44 +0700

    570610589 Lab1-2 three fn debug return value

--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -16,7 +16,7 @@ namespace Lab1_2
         static int[] inputfn()
         {
             int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
-     
+
             return input;
         }
 
@@ -37,7 +37,7 @@ namespace Lab1_2
                     }
                 }
             }
-            return 0;
+            return num;
         }
 
         static int[] outputfn(int[] output)
@@ -48,8 +48,8 @@ namespace Lab1_2
                 Console.Write(" ");
             }
             Console.ReadKey();
-            return 0;
+            return output;
         }
-
+        
     }
 }

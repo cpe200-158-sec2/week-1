527eb1fcde57b4cfd1e2e1690c5911d0db585247
Peeranut Pongpakatien <peeranut.31@hotmail.com>
2015-08-27 15:48:53 +0700

    570610589 Lab 1-2 three fn

--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -9,35 +9,47 @@ namespace Lab1_2
     class BubbleSortFunction
     {
         static void Main(string[] args)
+        {          
+            outputfn(processfn(inputfn()));          
+        }
+
+        static int[] inputfn()
+        {
+            int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
+     
+            return input;
+        }
+
+        static int[] processfn(int[] num)
         {
             bool flag = true;
-
-            // input
-            int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
-
-            // process
             while (flag)
             {
                 flag = false;
-                for (int i = 0; i != input.Length - 1; i++)
+                for (int i = 0; i != num.Length - 1; i++)
                 {
-                    if (input[i] > input[i + 1])
+                    if (num[i] > num[i + 1])
                     {
-                        int temp = input[i];
-                        input[i] = input[i + 1];
-                        input[i + 1] = temp;
+                        int temp = num[i];
+                        num[i] = num[i + 1];
+                        num[i + 1] = temp;
                         flag = true;
                     }
                 }
             }
+            return 0;
+        }
 
-            // output
-            for (int i = 0; i != input.Length; i++)
+        static int[] outputfn(int[] output)
+        {
+            for (int i = 0; i != output.Length; i++)
             {
-                Console.Write(input[i]);
+                Console.Write(output[i]);
                 Console.Write(" ");
             }
             Console.ReadKey();
+            return 0;
         }
+
     }
 }

3417acf7527d6d364c562459bec2dbe98bfcaf5c
thitiwat-buatip <subin2538@gmail.com>
2015-08-28 01:36:50 +0700

    lab1-2 570610565 Three functions

--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -16,10 +16,8 @@ namespace Lab1_2
         // input
         static int[] input()
         {
-            Console.WriteLine("Input your Numbers For Process :");
-            string recieve = Console.ReadLine();
-            int[] input = recieve.Split(' ').Select(integer => Convert.ToInt32(integer)).ToArray();
-            return input;
+            int[] i = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
+            return i;
         }
         // process
         static int[] process(int[] input)

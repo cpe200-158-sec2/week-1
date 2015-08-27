2a2f97f003354e0b1630ce7ac5d655fac82702fe
iamtarika <pairsir@outlook.com>
2015-08-27 15:28:44 +0700

    570610570 lab 1.2___(1)

--- Lab/Lab1-1/Program.cs
+++ Lab/Lab1-1/Program.cs
@@ -3,48 +3,5 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace Lab1_1
-{
-    class BubbleSort
-    {
-        static void Main(string[] args)
-        {
-           
-
-         
-            int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
-
-            
-            
-            for(int k=0;k!=input.Length-2;k++)
-            {
-
-                for (int i = 0; i != input.Length - 1; i++)
-                {
-                    if (input[i] < input[i + 1])
-                    {
-                        int temp = input[i];
-                        input[i] = input[i + 1];
-                        input[i + 1] = temp;
-                        
-                    }
-
-
-                }
-
-
-            }
-                
-            
-
-            // output
-            for (int i = 0; i != input.Length; i++)
-            {
-                Console.Write(input[i]);
-                Console.Write(" ");
-            }
-            Console.ReadKey();
-        }
-    }
-}
--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -4,40 +4,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab1_2
+
+
+namespace Lab1_1
 {
-    class BubbleSortFunction
+    class BubbleSort
     {
         static void Main(string[] args)
         {
-            bool flag = true;
 
-            // input
+
+            int[] int2 = input1();
+            processic(int2);
+            pim(int2);
+            
+            
+        }
+
+        static int[] input1()
+        {
             int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
 
-            // process
-            while (flag)
+            return input;
+
+        }
+
+       static void pim (int[] b)
+        {
+        for (int i = 0; i != b.Length; i++)
+    {
+            Console.Write(b[i]);
+            Console.Write(" ");
+    }
+        Console.ReadKey();
+        }
+
+
+        static void processic(int[] a)
+    {
+        for (int k = 0; k != a.Length - 1; k++)
+        {
+
+            for (int i = 0; i != a.Length - 1; i++)
             {
-                flag = false;
-                for (int i = 0; i != input.Length - 1; i++)
+                if (a[i] < a[i + 1])
                 {
-                    if (input[i] < input[i + 1])
-                    {
-                        int temp = input[i];
-                        input[i] = input[i + 1]; 
-                        input[i + 1] = temp;
-                        flag = true;
-                    }
+                    int temp = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+
                 }
+
+
             }
 
-            // output
-            for (int i = 0; i != input.Length; i++)
-            {
-                Console.Write(input[i]);
-                Console.Write(" ");
-            }
-            Console.ReadKey();
+
         }
     }
+
+}
 }

84dea81e8d7f18e7097b36059919444f06069871
ardinsia <pakpoomruj@gmail.com>
2015-08-27 21:33:03 +0700

    570610592 lab 1.2.2

--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -3,51 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace Lab1_2
 {
     class BubbleSortFunction
     {
         static int[] input()
+        {
+            Console.Write("How many number do you want to sort ? : ");
+            int num = int.Parse(Console.ReadLine());
+            int[] input = new int[num];
+            for (int i = 0; i != input.Length; i++)
             {
-            int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1};
-            return input;
+                Console.Write("Number {0} is: ", i + 1);
+                input[i] = int.Parse(Console.ReadLine());
             }
-        static void
-            process(int[]x)
+            return input;
+        }
+        static void process(int[] a)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                flag = false;
+                for (int i = 0; i != a.Length - 1; i++)
                 {
-                bool flag = true;
-                     while (flag)
+                    if (a[i] > a[i + 1])
                     {
-                         flag = false;
-                               for (int i = 0; i != x.Length - 1; i++)
-                                {
-                                      if (x[i] > x[i + 1])
-                                        {
-                                         int temp = x[i];
-                                         x[i] = x[i + 1];
-                                         x[i + 1] = temp;
-                                         flag = true;
-                                        }
-                                }
+                        int temp = a[i];
+                        a[i] = a[i + 1];
+                        a[i + 1] = temp;
+                        flag = true;
                     }
                 }
-        static void
-            output(int[]x)
-                 {
-                  for (int i = 0; i != x.Length; i++)
-                     {
-                     Console.Write(x[i]);
-                     Console.Write(" ");
-                     }
-                 }
-       static void
-            Main(string[] args)
-                 {
-                 int[] x = input();
-                 process(x);
-                 output(x);
-                 Console.ReadKey();
-                 }
-         }
+            }
+        }
+        static void output(int[] b)
+        {
+            Console.Write("Result: ");
+            for (int i = 0; i != b.Length; i++)
+            {
+                Console.Write(b[i]);
+                Console.Write(" ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] x = input();
+            process(x);
+            output(x);
+            Console.ReadKey();
+        }
+    }
 }
-    
